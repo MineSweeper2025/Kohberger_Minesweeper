104e2b569107a473b727cf6ccad8d28d1629bc31
Luca Kohberger <lkohberg@htl-steyr.ac.at>
2026-01-07 13:58:04 +0100

    Rudimentary version of the Main Site with difficulty selection made

--- Minesweeper/src/main/java/htl/steyr/minesweeper/HelloController.java
+++ Minesweeper/src/main/java/htl/steyr/minesweeper/HelloController.java
@@ -1,5 +1,6 @@
 package htl.steyr.minesweeper;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
@@ -10,5 +11,14 @@ public class HelloController {
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    public void onAnfaenger(ActionEvent actionEvent) {
+    }
+
+    public void onFortgeschritten(ActionEvent actionEvent) {
+    }
+
+    public void onProfi(ActionEvent actionEvent) {
     }
 }
